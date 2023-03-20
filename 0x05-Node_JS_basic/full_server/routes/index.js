2d5427414c05006e497f3682b99eb8491ce7b80d
Fadlurahman Lawal <70358533+kingfad@users.noreply.github.com>
2023-03-20 06:02:24 +0100

    Update index.js

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,4 +1,4 @@
-const express = require(express);
+const express = require('express');
 const AppController = require('../controllers/AppController');
 const StudentsController = require('../controllers/StudentsController');
 
